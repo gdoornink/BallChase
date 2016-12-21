acce0110ef8d231f59b924acc9ac1051be6e50b9
gdoornink <gregdoornink@gmail.com>
2016-12-20 22:17:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function ballClick() {
 	var ball = event.target;
-  if(ball.style.margin = "0px auto 0px 0px") {
+  if(ball.style.backgroundColor = "rgb(10,50,0)") {
 		ball.style.backgroundColor = "rgb(0,0,0)";
 		ball.style.borderColor = "rgba(0,0,0,0.2)";
 		ball.innerHTML = "Left";
