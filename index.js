1297fa53d5c4aeddd242be578f2b61dfdbf41e27
gdoornink <gregdoornink@gmail.com>
2016-12-20 21:51:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function ballClick() {
-var ball = event.target;
-ball.style.backgroundColor = "rgb(0,0,0)";
-ball.style.borderColor = "rgba(0,0,0,0.2)";
-ball.innerHTML = "Clicked!";
+	var ball = event.target;
+  if(ball.style.margin = "0px auto 0px 0px) {
+		ball.style.backgroundColor = "rgb(0,0,0)";
+		ball.style.borderColor = "rgba(0,0,0,0.2)";
+		ball.innerHTML = "Left";
+		} else {
+		ball.style.backgroundColor = "rgb(10,50,0)";
+		ball.style.borderColor = "rgba(10,50,0,0.2)";
+		ball.innerHTML = "Right";
+		}
 }
