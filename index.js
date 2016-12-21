18ddb8e9a809b8ddf9dfcb54f07373ef22c1e2d5
gdoornink <gregdoornink@gmail.com>
2016-12-20 22:44:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function ballClick() {
 	}
 	switch (ball.ballside) {
 		case "left":
-			ball.style.backgroundColor = "rgb(0,0,0)";
+			ball.style.backgroundColor = "rgba(0,0,0,0.9)";
 			ball.style.borderColor = "rgba(0,0,0,0.2)";
 			ball.innerHTML = "Right";
 			ball.style.margin = "0px 0px 0px auto";
 			ball.ballside = "right";
 			break;
 		case "right":
-			ball.style.backgroundColor = "rgb(10,50,0)";
+			ball.style.backgroundColor = "rgb(10,50,0,0.9)";
 			ball.style.borderColor = "rgba(10,50,0,0.2)";
 			ball.innerHTML = "Left";
 			ball.style.margin = "0px auto 0px 0px";
