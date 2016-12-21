ef160eaadc4f34b0b64a44eff257979b89b32aeb
gdoornink <gregdoornink@gmail.com>
2016-12-20 21:08:22 -0800

    Test JS: Ball1 --> black

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
 //}
 //
 //ball.innerHTML = localStorage.count;
-
+/*
 ball = document.getElementById("ball");
-var ballClick = new function() {
-//	localStorage.count = number(localStorage.coount) + 1;
+var ballClick = new Function() {
+	if(
 	document.write("Click");
 	left = 5 + Math.floor(85 * random());
 	top = 5 + Math.floor(85 * random());
 	ball.style.left = left + "%";
 	ball.style.top = top + "%";
-}
+}*/
+
+var one = document.getElementById("ball1");
+one.style.backgroundColor = "black";
