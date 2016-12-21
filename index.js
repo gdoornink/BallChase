9d5214e1558276a9e9d7b0beba794a2b1a8816c3
gdoornink <gregdoornink@gmail.com>
2016-12-20 21:42:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-//if(!localStorage.count) {
-//	localStorage.count = 0;
-//	alert("Welcome! Just click the ball.");
-//}
-//
-//ball.innerHTML = localStorage.count;
-/*
-ball = document.getElementById("ball");
-var ballClick = new Function() {
-	if(
-	document.write("Click");
-	left = 5 + Math.floor(85 * random());
-	top = 5 + Math.floor(85 * random());
-	ball.style.left = left + "%";
-	ball.style.top = top + "%";
-}*/
 function ballClick() {
-var one = document.getElementById("ball1");
-one.style.backgroundColor = "black";
-one.innerHTML = "Hi!";
+var ball = event.target;
+ball.style.backgroundColor = "black";
+ball.innerHTML = "Clicked!";
 }
