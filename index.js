a88c25e923af3a194515cdd672b07598f7d5bea8
gdoornink <gregdoornink@gmail.com>
2016-12-21 12:07:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,24 +5,33 @@ for (var i=0; i < balls.length; i++) {
 
 function ballClick() {
 	var ball = event.target;
-	if(!ball.ballside) {
-		ball.ballside = "left";
+	if(!ball.ballcolor) {
+		ball.ballcolor = "green";
 	}
 	if(ball.open = "yes") {
-		switch (ball.ballside) {
-			case "left":
+		switch (ball.ballcolor) {
+			case "green":
 				ball.style.backgroundColor = "rgba(0,0,0,0.9)";
 				ball.style.borderColor = "rgba(0,0,0,0.2)";
 				ball.innerHTML = "";
 				ball.style.margin = "0px 0px 0px auto";
-				ball.ballside = "right";
+				ball.ballcolor = "black";
+				if(window.innerHeight > window.innerWidth) {
+					ball.style.margin = "auto 0px 0px 0px";
+				} else {
+					ball.style.margin = "0px 0px 0px auto";
+				}
 				break;
-			case "right":
+			case "black":
 				ball.style.backgroundColor = "rgba(10,50,0,0.9)";
 				ball.style.borderColor = "rgba(10,50,0,0.2)";
 				ball.innerHTML = "";
-				ball.style.margin = "0px auto 0px 0px";
-				ball.ballside = "left";
+				ball.ballcolor = "green";
+				if(window.innerHeight > window.innerWidth) {
+					ball.style.margin = "0px 0px auto 0px";
+				} else {
+					ball.style.margin = "0px auto 0px 0px";
+				}
 				break;
 		}
 	}
