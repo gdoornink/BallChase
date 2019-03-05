74b544d4794b1dc0f705720336abd793da8b832b
gdoornink <gregdoornink@gmail.com>
2019-03-04 20:59:10 -0800

    Update tpu_estimate.js

--- tpu_estimate.js
+++ tpu_estimate.js
@@ -1,6 +1,5 @@
 var t = TrelloPowerUp.iframe();
 
-/*
 window.estimate.addEventListener('submit', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
@@ -9,7 +8,6 @@ window.estimate.addEventListener('submit', function(event){
     t.closePopup();
   });
 });
-*/
 
 t.render(function(){
   t.sizeTo('#estimate').done();
